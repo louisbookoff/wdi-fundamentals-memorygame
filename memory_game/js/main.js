66e43082bcd0d90cdbf2d07b7c5ae09fe0924e28
Louis Bookoff <Louis@Louiss-MacBook-Pro.local>
2018-01-20 15:28:53 -0500

    created game logic unit 10

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,7 +8,29 @@ var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo); */
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	 rank: "queen",
+	 suit: "hearts",
+	 cardImage: "images/queen-of-hearts.png",
+},
+{
+	 rank: "queen",
+	 suit: "diamonds",
+	 cardImage: "images/queen-of-diamonds.png",
+},
+{
+	 rank: "king",
+	 suit: "hearts",
+	 cardImage: "images/king-of-hearts.png",
+},
+{
+	 rank: "king",
+	 suit: "diamonds",
+	 cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 
 
@@ -22,11 +44,13 @@ var checkForMatch = function() {
 		alert("Sorry, try again."); 
 	}
 	/* } */
-	}
+}
 
 var flipCard = function(cardId) {
-		console.log("User flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 	};
 			flipCard(0);
 			flipCard(2);
