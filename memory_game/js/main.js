1c5e38e648440934c09a2920549698e6be4c3e7e
Louis Bookoff <Louis@Louiss-MacBook-Pro.local>
2018-01-21 10:10:57 -0500

    Final Memory Game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,7 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+//var cards = ["queen", "queen", "king", "king"];
+//var cardsInPlay = [];
 /* var flipCard = function(cardId) {
-var cardOne = cards[0];
+/*var cardOne = cards[0]; 
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 var cardTwo = cards[2];
@@ -37,29 +37,39 @@ var cardsInPlay = [];
 //create alert if cards don't match
 
 var checkForMatch = function() {
-	/* if (cardsInPlay.length === 2) { */
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+	//check to see if two card match and alert user
+	//this.setAttribute('src', cards[cardId].cardImage);
+	//code above instructed to be placed in wrong section
+	 if (cardsInPlay.length === 2) { 
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	  } else {
 		alert("Sorry, try again."); 
 	}
-	/* } */
+	} 
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+		var cardId= this.getAttribute('data-id');
+		//document.getAttribute(cards[cardId].cardImage);
+		this.setAttribute('src', cards[cardId].cardImage);
 		console.log("User flipped " + cards[cardId].rank);
 		cardsInPlay.push(cards[cardId].rank);
+		checkForMatch();
 		console.log(cards[cardId].cardImage);
-		console.log(cards[cardId].suit);
+		console.log(cards[cardId].suit); 
 	};
-			flipCard(0);
-			flipCard(2);
-	 checkForMatch(); 
+	
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 
-//First: create function that will store all steps related to selecting, or flipping over, a card. 
-//When the user flips a card over, you'll want to add that card to the array of cards that are in play
-//If the user has flipped over two cards, you'll want to check for a match.
+	}
+};
 
-//Second: you'll create a function to group together the logic to check to see if the two cards that the user has flipped over match each other.
-//You'll provide feedback to the user letting them know if the two cards match, or if they should try again.
+createBoard();
